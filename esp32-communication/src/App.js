3a668e89e59a42558b8d025f2fd00902818d4425
MESA 01 <you@example.com>
2024-10-17 13:38:20 -0300

    styling

--- esp32-communication/src/App.js
+++ esp32-communication/src/App.js
@@ -18,7 +18,7 @@ function App() {
 
   return (
     <div className='App'>
-      <h1 className="title">Configurar la capacidad</h1>
+      <h1 className="title">Capacidad</h1>
 
       <div className='distdiv'>
         <input
